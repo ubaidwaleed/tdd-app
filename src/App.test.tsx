a001abd55f5b19b835583c5ae6d31e6627268950
ubaidwaleed <ubaidwaleed1@gmail.com>
2022-10-13 17:03:20 +0500

    Initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders hello world in the document', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hello/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test world in the document',()=>{
+  render(<App/>);
+  const linkElement1=screen.getByText(/world/i)
+  expect(linkElement1).toBeInTheDocument();
+})
